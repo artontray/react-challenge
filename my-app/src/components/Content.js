c7e26f72399771691afc2f0d27fb7ad690485203
Damien <db.webapp@gmail.com>
2023-03-01 20:25:39 +0000

    Searching an artist challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -8,14 +8,25 @@ export class Content extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            posts: []
         }
     }
-    
+    handleChange = (e) => {
+        const name = e.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter((post)=>{
+            return post.name.toLowerCase().includes(name);
+        })
+        
+        this.setState({
+            posts: filteredPosts
+        })
+    }
     componentDidMount() {
         setTimeout(()=>{
             this.setState({
                 isLoaded: true,
+                posts: savedPosts,
             })
         }, 2000)
     }
@@ -25,11 +36,20 @@ export class Content extends Component {
             <div className={css.Content}>
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
-                    
+                    <form>
+                        <label htmlFor='searchinput'>Search</label>
+                        <input 
+                        type='search' 
+                        id='searchinput' 
+                        placeholder='By Author'
+                        onChange={(e) => this.handleChange(e)}
+                        />
+                        <h4>posts found {this.state.posts.length}</h4>
+                    </form>
                 </div>
                 <div className={css.SearchResults}>
                     {
-                        this.state.isLoaded ? <PostItem savedPosts={savedPosts} /> : <Loader />
+                        this.state.isLoaded ? <PostItem savedPosts={this.state.posts} /> : <Loader />
                     }
                 </div>
             </div>
